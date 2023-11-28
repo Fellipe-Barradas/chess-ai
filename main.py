d80436a54ecab5808eb987bcc358a535607f6d6e
Brunna Rocha <brunnarochapfs@gmail.com>
2023-11-28 17:06:25 -0300

    Fez contador de jogadas e de vez

--- main.py
+++ main.py
@@ -21,13 +21,11 @@ def check_if_is_possible_move(board, destination):
     return destination in [move.uci() for move in board.legal_moves]
 
 def update_element(window, i, j, color):
-    window[(i,j)].update(button_color = color)
-    
+    window[(i,j)].update(button_color=color)
 
-def get_button_color(i,j):
+def get_button_color(i, j):
     return "#7c7c7c" if (i + j) % 2 == 0 else "#cccccc"
 
-# Função para converter a posição do tabuleiro para a notação do chess
 def make_move(origin, destination):
     colluns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     rows = ['8', '7', '6', '5', '4', '3', '2', '1']
@@ -40,20 +38,17 @@ def make_move(origin, destination):
 
     return f"{origin_col}{origin_row}{destination_col}{destination_row}"
 
-# Função para desenhar o tabuleiro
 def draw_board_by_fen(fen):
-
     board = fen.split()[0]
     board = board.split('/')
     board = [list(row) for row in board]
-    
+
     layout = []
     for i in range(len(board)):
         row = []
         for j in range(len(board[i])):
             element = board[i][j]
 
-            
             if element.isdigit():
                 for k in range(int(element)):
                     row.append(sg.Button("", 
@@ -74,56 +69,87 @@ def draw_board_by_fen(fen):
                                   image_size=(60,60),
                                   button_color=(get_button_color(i,j))
                                   )
-                
+
                 row.append(piece)  
 
         layout.append(row)
 
     return layout
 
-
-## Main
-
 tabuleiro = chess.Board()
-
 tabuleiro_layout = draw_board_by_fen(tabuleiro.fen())      
+movements_layout = sg.Listbox(values=[], size=(10, 20), key='-MOVEMENTS-')
+turn_label = sg.Text('Turno do Jogador Branco', key='-TURN-')
+reset_button = sg.Button('Reiniciar Partida', key='-RESET-')
 
-window = sg.Window('Tabuleiro', 
-        [
-            [sg.Column(tabuleiro_layout, element_justification='center', key='-TABULEIRO-')],
-        ])
+layout = [
+    [
+        sg.Column(tabuleiro_layout, element_justification='center', key='-TABULEIRO-'),
+        sg.Column([[turn_label], [sg.Text('Movimentos')], [movements_layout], [reset_button]], element_justification='center')
+    ],
+]
+
+window = sg.Window('Tabuleiro', layout)
 
 selected_piece = None
+movements = []
+white_turn = True
 while True:                            
     event, values = window.read()    
 
     if event == sg.WIN_CLOSED:
         break
-    
-    if event:
-        if selected_piece is None:
-            selected_piece = event
+    if event == '-RESET-':
+        print("Resetando")
+        tabuleiro.reset()
+        movements = []
+        window['-MOVEMENTS-'].update(values=movements)
+        white_turn = True
+        turn_label.update('Turno do Jogador Branco')
+        for i in range(len(board)):
+                for j in range(len(board[i])):
+                    element = board[i][j]
+                    
+                    if element == ".":
+                        window[(i,j)].update(image_filename=pieces[""], 
+                                                image_size=(60,60),
+                                                button_color = get_button_color(i,j)
+                                            )
+                    else:
+                        window[(i,j)].update(image_filename=pieces[element],
+                                                button_color = get_button_color(i,j))
 
+    if event:
+        if event == '-RESET-':
+            # Reinicia a partida
+          pass
+
+        elif selected_piece is None:
+            selected_piece = event
             update_element(window, selected_piece[0], selected_piece[1], "#0B00EF")
         else:
-            
             origin = {'row': selected_piece[0], 'col': selected_piece[1]}
-            update_element(window,selected_piece[0], selected_piece[1], get_button_color(origin["row"], origin["col"]))
+            update_element(window, selected_piece[0], selected_piece[1], get_button_color(origin["row"], origin["col"]))
             
             destination = {'row': event[0], 'col': event[1]}
 
             move = make_move(origin, destination)
 
-            # Verifica se o movimento é um roque
-        
             if move in ['e1g1', 'e8g8', 'e1c1', 'e8c8']:
                 print("Roque")
                 tabuleiro.push_uci(move)
             else:
                 if check_if_is_possible_move(tabuleiro, move):
                     tabuleiro.push_uci(move)    
+                    movements.append(move)
+                    window['-MOVEMENTS-'].update(values=movements)
 
-                # Update the board
+                    # Alterna o turno
+                    white_turn = not white_turn
+
+                    # Atualiza a label de turno
+                    turn_label.update('Turno do Jogador Branco' if white_turn else 'Turno do Jogador Preto')
+
                 board = str(tabuleiro).split('\n')
                 for i in range(len(board)):
                     board[i] = board[i].split(' ')
